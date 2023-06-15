b3d996c47e1d8fb539b261ca4a2e80cfa3f132ca
Katsumi Kato <katzumi+github@gmail.com>
2023-06-15 21:16:03 +0900

    Use svg icons

---
+++ components/RunnSvgIcon.vue
@@ -0,0 +1,50 @@
+<template>
+  <svg width="20" height="20" viewBox="0 0 600 700" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" class="runn-icon">
+    <g transform="matrix(0.837956,0,0,0.837956,-109.966,91.2273)">
+        <g transform="matrix(1.6932,0,0,0.933057,-266.214,-33.1687)">
+            <path d="M383.734,315.248L361.062,290.238L287.591,354.124L310.263,379.134L383.734,315.248Z" style="fill:rgb(36,41,46);"/>
+        </g>
+        <g transform="matrix(1.6932,0,0,0.933057,-141.328,26.2343)">
+            <path d="M383.734,315.248L361.062,290.238L287.591,354.124L310.263,379.134L383.734,315.248Z" style="fill:rgb(36,41,46);"/>
+        </g>
+        <g transform="matrix(1.6932,0,0,0.933057,-17.1507,-33.1687)">
+            <path d="M383.734,315.248L361.062,290.238L287.591,354.124L310.263,379.134L383.734,315.248Z" style="fill:rgb(36,41,46);"/>
+        </g>
+        <g transform="matrix(1.6932,0,0,0.933057,107.165,-33.1687)">
+            <path d="M383.734,315.248L361.062,290.238L287.591,354.124L310.263,379.134L383.734,315.248Z" style="fill:rgb(36,41,46);"/>
+        </g>
+        <g transform="matrix(1.6932,0,0,0.750159,-265.529,100.003)">
+            <rect x="287.187" y="183.478" width="23.076" height="189.226" style="fill:rgb(36,41,46);"/>
+        </g>
+        <g transform="matrix(1.6932,0,0,0.750159,-141.042,100.242)">
+            <rect x="287.187" y="183.478" width="23.076" height="189.226" style="fill:rgb(36,41,46);"/>
+        </g>
+        <g transform="matrix(1.6932,0,0,0.750159,-16.555,100.003)">
+            <rect x="287.187" y="183.478" width="23.076" height="189.226" style="fill:rgb(36,41,46);"/>
+        </g>
+        <g transform="matrix(1.6932,0,0,0.750159,107.932,100.003)">
+            <rect x="287.187" y="183.478" width="23.076" height="189.226" style="fill:rgb(36,41,46);"/>
+        </g>
+        <g transform="matrix(1.6932,0,0,0.750159,232.419,100.003)">
+            <rect x="287.187" y="183.478" width="23.076" height="189.226" style="fill:rgb(36,41,46);"/>
+        </g>
+    </g>
+</svg>
+</template>
+
+<script>
+export default {
+  name: 'RunnSvgIcon',
+  props: {
+    // SVG props go here
+  },
+};
+</script>
+
+<style scoped>
+.runn-icon {
+  float: left;
+  background-color: #f3d95b;
+  margin: 0.3em;
+}
+</style>
